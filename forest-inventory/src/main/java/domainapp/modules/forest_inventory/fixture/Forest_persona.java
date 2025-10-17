bd8367fc4cc617bef634c6acd5ee0fb913c61825
Milan Vlaski <milan.vlaski321@gmail.com>
2025-10-17 11:20:23 +0200

    feat: add forest-inventory module that mvn builds

---
+++ forest-inventory/src/main/java/domainapp/modules/forest_inventory/fixture/Forest_persona.java
@@ -0,0 +1,105 @@
+package domainapp.modules.forest_inventory.fixture;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+import org.springframework.core.io.ClassPathResource;
+
+import org.apache.causeway.applib.services.clock.ClockService;
+import org.apache.causeway.applib.services.registry.ServiceRegistry;
+import org.apache.causeway.applib.value.Blob;
+import org.apache.causeway.testing.fakedata.applib.services.FakeDataService;
+import org.apache.causeway.testing.fixtures.applib.personas.BuilderScriptWithResult;
+import org.apache.causeway.testing.fixtures.applib.personas.Persona;
+import org.apache.causeway.testing.fixtures.applib.setup.PersonaEnumPersistAll;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.SneakyThrows;
+import lombok.val;
+import lombok.experimental.Accessors;
+
+import domainapp.modules.forest_inventory.forest.Forest;
+import domainapp.modules.forest_inventory.forest.Forests;
+import jakarta.inject.Inject;
+
+@RequiredArgsConstructor
+public enum Forest_persona implements Persona<Forest, Forest_persona.Builder> {
+
+    FOO("Foo", "Foo.pdf"),
+    BAR("Bar", "Bar.pdf"),
+    BAZ("Baz", null),
+    FRODO("Frodo", "Frodo.pdf"),
+    FROYO("Froyo", null),
+    FIZZ("Fizz", "Fizz.pdf"),
+    BIP("Bip", null),
+    BOP("Bop", null),
+    BANG("Bang", "Bang.pdf"),
+    BOO("Boo", null);
+
+    private final String name;
+    private final String contentFileName;
+
+    @Override
+    public Builder builder() {
+        return new Builder().setPersona(this);
+    }
+
+    @Override
+    public Forest findUsing(final ServiceRegistry serviceRegistry) {
+        return serviceRegistry.lookupService(Forests.class).map(x -> x.findByNameExact(name)).orElseThrow();
+    }
+
+    @Accessors(chain = true)
+    public static class Builder extends BuilderScriptWithResult<Forest> {
+
+        @Getter @Setter private Forest_persona persona;
+
+        @Override
+        protected Forest buildResult(final ExecutionContext ec) {
+
+            val simpleObject = wrap(simpleObjects).create(persona.name);
+
+            if (persona.contentFileName != null) {
+                val bytes = toBytes(persona.contentFileName);
+                val attachment = new Blob(persona.contentFileName, "application/pdf", bytes);
+                simpleObject.updateAttachment(attachment);
+            }
+
+            simpleObject.setLastCheckedIn(clockService.getClock().nowAsLocalDate().plusDays(fakeDataService.ints().between(-10, +10)));
+
+            return simpleObject;
+        }
+
+        @SneakyThrows
+        private byte[] toBytes(String fileName){
+            InputStream inputStream = new ClassPathResource(fileName, getClass()).getInputStream();
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+            int nRead;
+            byte[] data = new byte[16384];
+
+            while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+                buffer.write(data, 0, nRead);
+            }
+
+            return buffer.toByteArray();
+        }
+
+        // -- DEPENDENCIES
+
+        @Inject Forests simpleObjects;
+        @Inject ClockService clockService;
+        @Inject FakeDataService fakeDataService;
+    }
+
+    public static class PersistAll
+            extends PersonaEnumPersistAll<Forest, Forest_persona, Builder> {
+        public PersistAll() {
+            super(Forest_persona.class);
+        }
+    }
+
+
+}
---
+++ forest-inventory/src/main/java/domainapp/modules/forest_inventory/forest/Forests.java
@@ -0,0 +1,88 @@
+package domainapp.modules.forest_inventory.forest;
+
+import java.util.List;
+
+import org.apache.causeway.applib.annotation.Action;
+import org.apache.causeway.applib.annotation.ActionLayout;
+import org.apache.causeway.applib.annotation.DomainService;
+import org.apache.causeway.applib.annotation.PriorityPrecedence;
+import org.apache.causeway.applib.annotation.PromptStyle;
+import org.apache.causeway.applib.annotation.SemanticsOf;
+import org.apache.causeway.applib.query.Query;
+import org.apache.causeway.applib.services.repository.RepositoryService;
+import org.apache.causeway.persistence.jpa.applib.services.JpaSupportService;
+
+import lombok.RequiredArgsConstructor;
+
+import domainapp.modules.forest_inventory.ForestInventoryModule;
+import domainapp.modules.forest_inventory.types.Name;
+import jakarta.annotation.Priority;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.persistence.TypedQuery;
+
+@Named(ForestInventoryModule.NAMESPACE + ".SimpleObjects")
+@DomainService
+@Priority(PriorityPrecedence.EARLY)
+@RequiredArgsConstructor(onConstructor_ = {@Inject} )
+public class Forests {
+
+    final RepositoryService repositoryService;
+    final JpaSupportService jpaSupportService;
+    final ForestRepository forestRepository;
+
+
+    @Action(semantics = SemanticsOf.NON_IDEMPOTENT)
+    @ActionLayout(promptStyle = PromptStyle.DIALOG_SIDEBAR)
+    public Forest create(
+            @Name final String name) {
+        return repositoryService.persist(Forest.withName(name));
+    }
+
+
+    @Action(semantics = SemanticsOf.NON_IDEMPOTENT)
+    @ActionLayout(promptStyle = PromptStyle.DIALOG_SIDEBAR)
+    public List<Forest> findByNameLike(
+            @Name final String name) {
+        return repositoryService.allMatches(
+                Query.named(Forest.class, Forest.NAMED_QUERY__FIND_BY_NAME_LIKE)
+                     .withParameter("name", "%" + name + "%"));
+    }
+
+
+    @Action(semantics = SemanticsOf.SAFE)
+    @ActionLayout(promptStyle = PromptStyle.DIALOG_SIDEBAR)
+    public List<Forest> findByName(
+            @Name final String name
+            ) {
+        return forestRepository.findByNameContaining(name);
+    }
+
+
+    public Forest findByNameExact(final String name) {
+        return forestRepository.findByName(name);
+    }
+
+
+
+    @Action(semantics = SemanticsOf.SAFE)
+    public List<Forest> listAll() {
+        return forestRepository.findAll();
+    }
+
+
+
+    public void ping() {
+        jpaSupportService.getEntityManager(Forest.class)
+            .mapEmptyToFailure()
+            .mapSuccessAsNullable(entityManager -> {
+                final TypedQuery<Forest> q = entityManager.createQuery(
+                                "SELECT p FROM Forest p ORDER BY p.name",
+                                Forest.class)
+                        .setMaxResults(1);
+                return q.getResultList();
+            })
+        .ifFailureFail();
+    }
+
+}
