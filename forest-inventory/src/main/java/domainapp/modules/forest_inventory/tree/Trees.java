042b95f9f12b9a84df3726387318c841d6363077
Milan Vlaski <milan.vlaski321@gmail.com>
2025-10-30 11:58:17 +0100

    fix: addTree function, removed Forest and Inventory from it.

--- forest-inventory/src/main/java/domainapp/modules/forest_inventory/tree/Trees.java
+++ forest-inventory/src/main/java/domainapp/modules/forest_inventory/tree/Trees.java
@@ -20,8 +20,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.val;
 
 import domainapp.modules.forest_inventory.ForestInventoryModule;
-import domainapp.modules.forest_inventory.forest.Forest;
-import domainapp.modules.forest_inventory.inventory.Inventory;
 import domainapp.modules.forest_inventory.plot.Plot;
 import domainapp.modules.forest_inventory.tree.condition.Condition;
 import domainapp.modules.forest_inventory.tree.species.Species;
@@ -56,15 +54,17 @@ public class Trees {
             final Condition condition,
             @Parameter(optionality = Optionality.OPTIONAL) @ParameterLayout
             final String notes,
-            @Parameter(optionality = Optionality.OPTIONAL) @ParameterLayout
-            final Forest forest,
-            @Parameter(optionality = Optionality.OPTIONAL) @ParameterLayout
-            final Inventory inventory,
+//            @Parameter(optionality = Optionality.OPTIONAL) @ParameterLayout
+//            final Forest forest,
+//            @Parameter(optionality = Optionality.OPTIONAL) @ParameterLayout
+//            final Inventory inventory,
             @Parameter(optionality = Optionality.OPTIONAL) @ParameterLayout
             final Plot plot
     ) {
         val tree = new Tree(dbh, height, species, condition, notes);
-        plot.addTree(tree);
+        if(plot != null) {
+            plot.addTree(tree);
+        }
         return repositoryService.persist(tree);
     }
 
@@ -78,18 +78,18 @@ public class Trees {
         return repositoryService.allInstances(Condition.class);
     }
 
-    @MemberSupport
-    public java.util.Collection<Forest> choices5AddTree() {
-        return repositoryService.allInstances(Forest.class);
-    }
+//    @MemberSupport
+//    public java.util.Collection<Forest> choices5AddTree() {
+//        return repositoryService.allInstances(Forest.class);
+//    }
+//
+//    @MemberSupport
+//    public java.util.Collection<Inventory> choices6AddTree() {
+//        return repositoryService.allInstances(Inventory.class);
+//    }
 
     @MemberSupport
-    public java.util.Collection<Inventory> choices6AddTree() {
-        return repositoryService.allInstances(Inventory.class);
-    }
-
-    @MemberSupport
-    public java.util.Collection<Plot> choices7AddTree() {
+    public java.util.Collection<Plot> choices5AddTree() {
         return repositoryService.allInstances(Plot.class);
     }
 }
