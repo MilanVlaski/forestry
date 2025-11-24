57d43053096ca74fdbfb7d41ad8bab513ff91a37
Milan Vlaški <milan.vlaski321@gmail.com>
2025-11-24 13:50:18 +0100

    feat: remove unnecessary dependencies

--- webapp/src/main/java/domainapp/webapp/AppManifest.java
+++ webapp/src/main/java/domainapp/webapp/AppManifest.java
@@ -15,15 +15,10 @@ import org.apache.causeway.applib.CausewayModuleApplibMixins;
 import org.apache.causeway.core.config.presets.CausewayPresets;
 import org.apache.causeway.core.metamodel.inspect.CausewayModuleCoreMetamodelMixins;
 import org.apache.causeway.core.runtimeservices.CausewayModuleCoreRuntimeServices;
-import org.apache.causeway.extensions.audittrail.jpa.CausewayModuleExtAuditTrailPersistenceJpa;
-import org.apache.causeway.extensions.commandlog.jpa.CausewayModuleExtCommandLogPersistenceJpa;
-import org.apache.causeway.extensions.executionlog.jpa.CausewayModuleExtExecutionLogPersistenceJpa;
-import org.apache.causeway.extensions.executionoutbox.jpa.CausewayModuleExtExecutionOutboxPersistenceJpa;
 import org.apache.causeway.extensions.flyway.impl.CausewayModuleExtFlywayImpl;
 import org.apache.causeway.extensions.pdfjs.wkt.ui.CausewayModuleExtPdfjsWicketUi;
 import org.apache.causeway.extensions.secman.encryption.spring.CausewayModuleExtSecmanEncryptionSpring;
 import org.apache.causeway.extensions.secman.jpa.CausewayModuleExtSecmanPersistenceJpa;
-import org.apache.causeway.extensions.sessionlog.jpa.CausewayModuleExtSessionLogPersistenceJpa;
 import org.apache.causeway.extensions.tabular.excel.CausewayModuleExtTabularExcel;
 import org.apache.causeway.persistence.jpa.eclipselink.CausewayModulePersistenceJpaEclipselink;
 import org.apache.causeway.testing.fixtures.applib.CausewayModuleTestingFixturesApplib;
@@ -37,6 +32,7 @@ import org.apache.causeway.viewer.wicket.viewer.CausewayModuleViewerWicketViewer
 import domainapp.webapp.application.ApplicationModule;
 import domainapp.webapp.application.fixture.scenarios.DomainAppDemo;
 import domainapp.webapp.custom.CustomModule;
+
 
 @Configuration
 @Import({
@@ -60,11 +56,12 @@ import domainapp.webapp.custom.CustomModule;
 
         CausewayModuleExtSecmanPersistenceJpa.class,
         CausewayModuleExtSecmanEncryptionSpring.class,
-        CausewayModuleExtSessionLogPersistenceJpa.class,
-        CausewayModuleExtAuditTrailPersistenceJpa.class,
-        CausewayModuleExtCommandLogPersistenceJpa.class,
-        CausewayModuleExtExecutionLogPersistenceJpa.class,
-        CausewayModuleExtExecutionOutboxPersistenceJpa.class,
+
+//        CausewayModuleExtSessionLogPersistenceJpa.class,
+//        CausewayModuleExtAuditTrailPersistenceJpa.class,
+//        CausewayModuleExtCommandLogPersistenceJpa.class,
+//        CausewayModuleExtExecutionLogPersistenceJpa.class,
+//        CausewayModuleExtExecutionOutboxPersistenceJpa.class,
 
         CausewayModuleExtLayoutLoadersGithub.class,
         CausewayModuleExtTabularExcel.class,
